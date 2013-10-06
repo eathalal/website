a24494ad3807a9907921a2e7d65b2258ee7fbe09
Eat Halal <eathalalltd@gmail.com>
2013-10-06 11:48:15 +0100

    Adds categories#index and makes it the root page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Website::Application.routes.draw do
+  get "categories/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'categories#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

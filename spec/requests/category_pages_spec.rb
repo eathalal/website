1257d5532ccf0ad014cb2d0d91751771e2ee99cd
Eat Halal <eathalalltd@gmail.com>
2013-10-06 12:14:37 +0100

    Sets default title and modifies categories#index view.

---
+++ spec/requests/category_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "Category pages" do
+
+  describe "index" do
+
+    it "should have the content 'Eat Halal'" do
+      visit root_path
+      expect(page).to have_content('Eat Halal')
+    end
+
+    it "should have the right title" do
+  		visit root_path
+  		expect(page).to have_title("Eat Halal")
+		end
+  end
+end
+
+

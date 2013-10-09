54f79a2bd147cecb8810642f26f9316d90577aff
Eat Halal <eathalalltd@gmail.com>
2013-10-09 08:56:18 +0100

    Displays posts in blog index.

--- spec/requests/post_pages_spec.rb
+++ spec/requests/post_pages_spec.rb
@@ -3,10 +3,20 @@ require 'spec_helper'
 describe "Post pages" do
 
   describe "index" do
+    let(:post_1) { FactoryGirl.create(:post) }
 
     it "should have the right title" do
       visit blog_path
       expect(page).to have_title("Eat Halal | On The Block")
+    end
+
+    context "The is one published post" do
+      it "should display the post" do
+        post_1
+        visit blog_path
+        expect(page).to have_text post_1.title
+        expect(page).to have_text post_1.content 
+      end      
     end
   end
 end
